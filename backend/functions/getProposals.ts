30c1f302abf8c26b16d623593fb4d5f380c501be
Keating <keating.dev@protonmail.com>
2022-06-29 11:53:40 -0400

    Add some more endpoints

--- backend/functions/getProposals.ts
+++ backend/functions/getProposals.ts
@@ -1,65 +1,117 @@
-import { APIGatewayProxyHandlerV2 } from 'aws-lambda'
-import fetch from 'node-fetch'
+import { APIGatewayProxyHandlerV2 } from "aws-lambda";
+import fetch from "node-fetch";
 
 function apiRequest(path: string, method: string, data: any) {
-    return fetch(path, {
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        method,
-        redirect: 'follow',
-        body: JSON.stringify(data),
-    }).then((res) => res.json())
+  return fetch(path, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method,
+    redirect: "follow",
+    body: JSON.stringify(data),
+  }).then((res) => res.json());
 }
+
+const calculateSatatus = (
+  processed: boolean,
+  cancelled: boolean,
+  passed: boolean,
+  votingStarts: number,
+  votingEnds: number,
+  graceEnds: number,
+  sponsored: boolean
+) => {
+  const now = Date.now();
+  if (processed) {
+    return "processed";
+  } else if (passed) {
+    return "passed";
+  } else if (cancelled) {
+    return "cancelled";
+  } else if (now >= votingEnds && graceEnds > now) {
+    return "grace";
+  } else if (now >= votingStarts && votingEnds > now) {
+    return "voting";
+  } else if (sponsored) {
+    return "sponsored";
+  } else {
+    return "submitted";
+  }
+};
 
 export const handler: APIGatewayProxyHandlerV2 = async (event) => {
-    const eventId = event?.pathParameters?.id
-    if (!eventId) return { statusCode: 400 }
+  const eventId = event?.pathParameters?.id;
+  if (!eventId) return { statusCode: 400 };
 
-    const template = {
-        '@context': {
-            '@vocab': 'http://daostar.org/',
-        },
-        type: 'DAO',
-        name: eventId,
-    }
+  const template = {
+    "@context": {
+      "@vocab": "http://daostar.org/",
+    },
+    type: "DAO",
+    name: eventId,
+  };
 
-    const query = `query GetMembers($dao: String!) {
-        memberUri(id: $dao) {
+  const query = `query GetProposals($dao: String!) {
+        proposalUri(id: $dao) {
           id
-          members {
-            memberAddress
+          proposals {
+            id
+						title
+						contentUri
+						processed
+            cancelled
+            passed
+            votingStarts
+            votingEnds
+            graceEnds
+						sponsored
           }
         }
-      }`
+      }`;
 
-    const path = 'https://api.thegraph.com/subgraphs/name/alexkeating/daostar-moloch'
+  const path =
+    "https://api.thegraph.com/subgraphs/name/alexkeating/daostar-moloch";
 
-    const data = {
-        query,
-        variables: { dao: eventId },
-    }
+  const data = {
+    query,
+    variables: { dao: eventId },
+  };
 
-    const res = (await apiRequest(path, 'POST', data)) as any
-    console.log({ res })
+  const res = (await apiRequest(path, "POST", data)) as any;
+  console.log({ res });
 
-    const members = res.data.memberUri.members
+  const proposals = res.data.proposalUri.proposals;
 
-    console.log({ members })
+  console.log({ proposals });
 
-    const membersFormatted = members.map((m: any) => {
-        return { id: m.memberAddress, type: 'EthereumAddress' }
-    })
+  const proposalsFormatted = proposals.map((p: any) => {
+    const status = calculateSatatus(
+      p.processed,
+      p.cancelled,
+      p.passed,
+      p.votingStarts,
+      p.votingEnds,
+      p.graceEnds,
+      p.sponsored
+    );
+    return {
+      id: p.id,
+      type: "proposal",
+      name: p.title,
+      contentURI: p.contentURI,
+      status: status,
+    };
+  });
 
-    const transformed = { members: membersFormatted, ...template }
+  const transformed = { proposals: proposalsFormatted, ...template };
 
-    return transformed
-        ? {
-              statusCode: 200,
-              body: JSON.stringify(transformed),
-          }
-        : {
-              statusCode: 404,
-              body: JSON.stringify({ error: true }),
-          }
-}
+  return transformed
+    ? {
+        statusCode: 200,
+        body: JSON.stringify(transformed),
+      }
+    : {
+        statusCode: 404,
+        body: JSON.stringify({ error: true }),
+      };
+};
--- stacks/MyStack.ts
+++ stacks/MyStack.ts
@@ -1,21 +1,22 @@
-import { Api, StackContext } from '@serverless-stack/resources'
+import { Api, StackContext } from "@serverless-stack/resources";
 
 export function MyStack({ stack }: StackContext) {
-    // Create the HTTP API
-    const api = new Api(stack, 'Api', {
-        routes: {
-            'GET /members/{id}': 'functions/getMembers.handler',
-            'GET /proposals/{id}': 'functions/getProposals.handler',
-        },
-        // customDomain: {
-        //     domainName: "sandbox.logos.xyz",
-        //     hostedZone: "sandbox.logos.xyz",
-        //     path: "api/v1",
-        // },
-    })
+  // Create the HTTP API
+  const api = new Api(stack, "Api", {
+    routes: {
+      "GET /members/{id}": "functions/getMembers.handler",
+      "GET /proposals/{id}": "functions/getProposals.handler",
+      "GET /acitvities/{id}": "functions/getActivityLogs.handler",
+    },
+    // customDomain: {
+    //     domainName: "sandbox.logos.xyz",
+    //     hostedZone: "sandbox.logos.xyz",
+    //     path: "api/v1",
+    // },
+  });
 
-    // Show the API endpoint in the output
-    stack.addOutputs({
-        ApiEndpoint: api.url,
-    })
+  // Show the API endpoint in the output
+  stack.addOutputs({
+    ApiEndpoint: api.url,
+  });
 }
