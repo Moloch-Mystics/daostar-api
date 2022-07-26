26b5f4329c85bc2b02d8e905f08cb321deed6659
Isaac Patka <ipatka@gmail.com>
2022-07-26 16:57:13 -0400

    v2 routes

--- stacks/MyStack.ts
+++ stacks/MyStack.ts
@@ -1,22 +1,25 @@
-import { Api, StackContext } from "@serverless-stack/resources";
+import { Api, StackContext } from '@serverless-stack/resources'
 
 export function MyStack({ stack }: StackContext) {
-  // Create the HTTP API
-  const api = new Api(stack, "Api", {
-    routes: {
-      "GET /members/{id}": "functions/getMembers.handler",
-      "GET /proposals/{id}": "functions/getProposals.handler",
-      "GET /activities/{id}": "functions/getActivityLogs.handler",
-    },
-    customDomain: {
-        domainName: "sandbox.logos.xyz",
-        hostedZone: "sandbox.logos.xyz",
-        path: "api/v1",
-    },
-  });
+    // Create the HTTP API
+    const api = new Api(stack, 'Api', {
+        routes: {
+            'GET /molochv2/members/{network}/{id}': 'functions/molochv2/getMembers.handler',
+            'GET /molochv2/proposals/{network}/{id}': 'functions/molochv2/getProposals.handler',
+            'GET /molochv2/activities/{network}/{id}': 'functions/molochv2/getActivityLogs.handler',
+            'GET /molochv3/members/{network}/{id}': 'functions/molochv3/getMembers.handler',
+            'GET /molochv3/proposals/{network}/{id}': 'functions/molochv3/getProposals.handler',
+            'GET /molochv3/activities/{network}/{id}': 'functions/molochv3/getActivityLogs.handler',
+        },
+        // customDomain: {
+        //     domainName: 'sandbox.logos.xyz',
+        //     hostedZone: 'sandbox.logos.xyz',
+        //     path: 'api/v1',
+        // },
+    })
 
-  // Show the API endpoint in the output
-  stack.addOutputs({
-    ApiEndpoint: api.url,
-  });
+    // Show the API endpoint in the output
+    stack.addOutputs({
+        ApiEndpoint: api.url,
+    })
 }
